6bfae59fe23ce2ea7dd2a7f8c784e0cf80847c12
zStrikke <zstrikke@gmail.com>
2022-08-31 21:17:13 +0200

    Adding Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// linktr.test/dashboard
+Route::group(['middleware' => 'auth', 'prefix' => 'dashboard'], function() {
+    Route::get('/links', 'LinkController@index');
+    Route::get('/links/new', 'LinkController@create');
+    Route::post('/links/new', 'LinkController@store');
+    Route::get('/links/{link}', 'LinkController@edit');
+    Route::post('/links/{link}', 'LinkController@update');
+    Route::delete('/links/{links}', 'LinkController@destroy');
+
+    //Route::resource...
+
+    Route::get('/settings', 'UserController@edit');
+    Route::post('/settings', 'UserController@update');
+    
+});
+
+Route::post('/visit/{link}', 'VisitController@store');
+Route::get('/{user}', 'UserController@show');
